e91f707129329c96ff67aa41c736f2c1ab19da91
Lutias <01joshua@gmail.com>
2021-05-13 16:34:58 -0700

    update sw.js and script.js

--- sw.js
+++ sw.js
@@ -4,3 +4,38 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+const CACHE_NAME = 'journal-cache';
+var urlsToCache = [
+    'https://cse110lab6.herokuapp.com/entries'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Found in cache, return response
+          if (response) {
+            return response;
+          }
+          // Try to fetch the default network request if not found in cache
+          return fetch(event.request);
+        }
+      )
+  );
+});
